c1955d8a670e4e02804f2c3c7d9b2ab94f532cb3
Cesar Medrano <cesardedios94@gmail.com>
2018-11-06 21:51:42 -0500

    Getting import lib issue but I'll fix it later, fixed bugs now working

--- decisionMaking.py
+++ decisionMaking.py
@@ -1,9 +1,12 @@
 from sklearn import tree
+import importlib
 
 # [height, weight, shoe size]
 X = [[181,80,44], [117,70,43], [160,60,38], [154,54,37],
      [166,65,40], [190,90,47], [175,64,39], [177,70,40],
      [159,55,37], [171,75,42], [181,85,43] ]
+
+# X = [ int(x) for x in X]
 
 # Genders
 Y = ['male', 'female', 'female', 'female', 'male', 'male',

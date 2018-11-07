ce026b36a4bea1c6a8bdeae1dc069eb1f27bf2af
Cesar Medrano <cesardedios94@gmail.com>
2018-11-06 21:28:51 -0500

    finished setting up traingin model

--- decisionMaking.py
+++ decisionMaking.py
@@ -1,6 +1,22 @@
 from sklearn import tree
 
-#[height, weight, shoe size]
+# [height, weight, shoe size]
 x = [[181,80,44], [117,70,43], [160,60,38], [154,54,37]
      [166,65,40], [190,90,47], [175,64,39], [177,70,40],
      [159,55,37], [171,75,42], [181,85,43] ]
+
+# Genders
+Y = ['male', 'female', 'female', 'female', 'male', 'male',
+     'male', 'female', 'male', 'female', 'male']
+
+# initializing decision tree object
+clf = tree.DecisionTreeClassifier()
+
+# Decision tree var for training model
+clf = clf.fit(X,Y)
+
+# variable for storing prediction to test model
+predition = clf.predict([[190,70,43]])
+
+# Print to terminal
+print prediction
